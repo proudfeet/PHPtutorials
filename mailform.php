47dc48bcec8682e293372b31f01ee25279044ea8
proudfeet <simon.d.proudfoot@gmail.com>
2014-07-03 21:56:47 -0400

    email successfully sent, still needs some work, need to inject some if statements to control whether or not emails meet all criteria, also need to figure out why email sends to copies

--- mailform.php
+++ mailform.php
@@ -4,11 +4,21 @@
 	$last = htmlspecialchars($_POST['lastName']);
 	$fullName = $first . " " . $last;
 	$message = htmlspecialchars($_POST['message']);
+	$sendto = "simon.d.proudfoot@gmail.com";
+	$subject = "wanted to tell you something...";
+	$header = "From: " . strip_tags($_POST['req-email']);
 
- ?>
+
+	mail($sendto, $fullName . " " . $subject, $message, $header)
+
+?>
 
 <h1>Hey!</h1>
 
 <h2><?php echo $fullName ?> has news for you! He told me to tell you:</h2>
 
 <p><?php echo $message ?></p>
+
+<p>But you already knew that because your message has been sent to <?php echo $sendto ?></p>
+
+
