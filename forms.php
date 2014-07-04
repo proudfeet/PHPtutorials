db2da55b8acf38d52d2b87bf55e23e71839e8005
proudfeet <simon.d.proudfoot@gmail.com>
2014-07-03 20:43:47 -0400

    cleaned up PHP a bit

--- forms.php
+++ forms.php
@@ -1,2 +1,11 @@
-<p>Hi there <?php echo htmlspecialchars($_POST['name']); ?>.</p>
-<p>I see that you are <?php echo (int)$_POST['age']; ?> years old.</p>
+<?php
+
+$name = htmlspecialchars($_POST['name']);
+$age = (int)$_POST['age'];
+
+?>
+
+
+<p>Hi there <?php echo $name ?>.</p>
+<!-- htmlspecialchars is a built-in function to PHP that ensures that no one adds special characters to their inputted information, stopping someone from injecting HTML or JavaScript into the form -->
+<p>I see that you are <?php echo $age ?> years old.</p>
