a0808d2f0fcb0518910034d513a3525e2d53e466
proudfeet <simon.d.proudfoot@gmail.com>
2014-07-05 22:06:58 -0400

    fibonacci sequence created, now going to create a program that finds a specific term in the sequence

--- fibonacci.php
+++ fibonacci.php
@@ -3,4 +3,19 @@
 
 
 
+function fibonacci($n, $first = 0, $second = 1)
+// Must declare this as a function, as it will not run properly otherwise
+
+{
+  $start = [$first, $second];
+  for($i=1;$i<$n;$i++)
+  {
+    $fib[] = $fib[$i]+$fib[$i-1];
+  }
+  return $fib;
+}
+echo "<pre>";
+print_r(fibonacci(50));
+// echo "</pre>";
+
 ?>
