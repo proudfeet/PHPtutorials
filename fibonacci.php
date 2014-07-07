0f57a22775409d0fbdc79a461d1c8a39d4ff31c5
proudfeet <simon.d.proudfoot@gmail.com>
2014-07-07 00:11:25 -0400

    iterative method done, not printing, can't figure out why

--- fibonacci.php
+++ fibonacci.php
@@ -1,5 +1,7 @@
 <?php 
 
+// The recursive method, displays each iteration of the fibonacci sequence until the desired term
+
 $term = intval($_POST['fibterm']);
 
 function fibonacci($term, $first = 0, $second = 1)
@@ -13,13 +15,35 @@ function fibonacci($term, $first = 0, $second = 1)
   }
   return $sequence;
 }
-
-
-
 echo "<pre>";
-echo "Function";
 print_r(fibonacci($term));
 echo "</pre>";
 
+?>
+
+<?php 
+
+// The iterative method, shows only the term requested
+
+$term = intval($_POST['fibterm']);
+
+function fibonacciIter($term) {
+
+
+	$index1 = 1;
+	$index2 = 1;
+	$fib = 0;
+
+	for ($i=1; $i<$term;$i++) 
+	{ 
+		$fib = $index1 + $index2;	
+		$index2 = $index1;
+		$index1 = $fib;
+	}
+
+	print_r($fib);
+
+}
 
 ?>
+
