ea39604d64680e27fcc6210b612de086f0bd5956
proudfeet <simon.d.proudfoot@gmail.com>
2014-07-13 23:54:26 -0400

    connection made to mysql database, not totally working

--- connectedSignUp.php
+++ connectedSignUp.php
@@ -1,7 +1,15 @@
 <?php 
-	$link = mysql_connect('localhost', 'root', 'root');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'loginpractice');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
 
-
-
+	$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+	$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+	if (mysqli_connect_errno($link)) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_errno();
+	} else {
+		echo "Successfully connected to your database";
+	}
 
 ?>
